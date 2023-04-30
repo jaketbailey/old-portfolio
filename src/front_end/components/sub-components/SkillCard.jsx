737e0b09c4de5e28635bdf9c98df256d06eea685
Jake Bailey - UP2002753 <jake.bailey2801@gmail.com>
2023-04-30 13:37:17 +0100

    Skill Links added and flexbox fixed for mobile Charity/Achievements sections

--- src/front_end/components/sub-components/SkillCard.jsx
+++ src/front_end/components/sub-components/SkillCard.jsx
@@ -1,13 +1,15 @@
 
 export default function SkillCard (props) {
   return (
-    <div class="custom">
-      <div class="card skill-card ">
-        <div class="card-body-skill">
-          <img class="skill-icon" src={props.src}/>
-          {props.skillName}
+    <div className ="custom">
+      <a className ="skill-link" href={props.link} target="_blank">
+        <div className ="card skill-card ">
+          <div className ="card-body-skill">
+              <img className ="skill-icon" src={props.src}/>
+              {props.skillName}
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   )
 }
