9011231e8fcf2435088b18b828529f38a44a8421
Jake Bailey - UP2002753 <jake.bailey2801@gmail.com>
2023-11-12 16:36:25 +0000

    final

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const next = require('next');
+const server = express();
+server.use(express.json());
+
+function init() {
+  console.log('Starting Server');
+
+  const dev = process.env.NODE_ENV !== 'production';
+  const app = next({dev, dir: './'});
+  const handle = app.getRequestHandler();
+  const port = 80;
+
+  app.prepare().then(() => {
+    server.all('*', (req, res) => {
+      return handle(req, res);
+    });
+  
+    server.listen(port, (err) => {
+      if (err) console.error(err);
+      console.log(`Server listening on port: ${port}`);
+    });
+  })
+}
+
+module.exports = {
+  App: server,
+  Init: init,
+};
